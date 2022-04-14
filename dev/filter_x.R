bcfd63957b212432cad25860c5c13f25de011701
Charlotte Dawson <csdaw@outlook.com>
2022-04-14 17:21:55 +0100

    Flesh out `filter_x()` and rename

--- dev/filter_x.R
+++ dev/filter_x.R
@@ -8,8 +8,8 @@ e_data <- matrix(
   dimnames = list(c("A", "B", "C"), c("sample1", "sample2"))
 )
 
-filter_x <- function(x, type = c("na", "zero", "sum"),
-                     pattern, op = c("==", "<=", "<", ">=", ">", "!="), value, ...) {
+rowsum2 <- function(x, type = c("na", "zero", "sum"),
+                    pattern, op = c("==", "<=", "<", ">=", ">", "!="), value, ...) {
   # check arguments
   match.arg(op)
   match.arg(type)
@@ -37,28 +37,74 @@ filter_x <- function(x, type = c("na", "zero", "sum"),
   # get operator function
   op <- match.fun(op)
 
-  # subset x using operator to compare calculated values versus given value
-  subset(x, op(test_values, value))
+  # compare calculated values for each row versus the desired value
+  op(test_values, value)
 }
 
-aaa <- filter_x(e_data, "sum", "sample1", ">", 0)
-bbb <- filter_x(e_data, "sum", "sample2", "<", 5)
+aaa <- rowsum2(e_data, "sum", "sample", "<", 6)
+bbb <- rowsum2(e_data, "sum", "sample", ">", 3)
 
 
-ccc <- mapply(
-  FUN = function(p, o, v) filter_x(e_data, "sum", p, o, v),
-  c("sample1", "sample2"),
-  c(">", "<"),
-  c(0, 5),
-  SIMPLIFY = FALSE,
-  USE.NAMES = FALSE
+filter_x <- function(x, type = c("na", "zero", "sum"),
+                     pattern, op, value, setop = NULL, ...) {
+  # check arguments
+  match.arg(type)
+  if(!any(class(x) %in% c("data.frame", "matrix")))
+    stop("x must be a data.frame or a matrix")
+
+  # repeat if args are longer than single string
+  if (any(lengths(list(pattern, op, value)) > 1)) {
+    # check argument
+    match.arg(setop, c("&", "|", "xor"))
+    if(missing(setop)) stop("You must specify 'setop'")
+
+    # generate list of logical vectors indicating which rows pass threshold
+    passing_rows <- mapply(function(p, o, v) rowsum2(x, type, p, o, v, ...),
+                           pattern, op, value, SIMPLIFY = FALSE)
+
+    # combine list of logical vectors by the specified setop
+    rows_to_keep <- Reduce(as.name(setop), passing_rows)
+  } else {
+    rows_to_keep <- rowsum2(x, type, pattern, op, value, ...)
+  }
+
+  # output filtered matrix/data.frame
+  x[rows_to_keep, , drop = FALSE]
+}
+
+microbenchmark::microbenchmark(
+  filter_x(e_data, "sum", "sample", "==", c(5, 7, 8, 9, 19, 11), setop = "xor"),
+  filter_x(e_data, "sum", "sample", ">", 3),
+  filterNA(e_data, pNA = 0, pattern = "01"),
+  times = 1000L
 )
 
-Reduce(union, ccc)
-matrix(Reduce(setdiff, ccc), ncol = 2)
+microbenchmark::microbenchmark(
+  filter_x(e_data, "na", "sample", "<", 1),
+  filterNA(e_data, pNA = 0, pattern = "01"),
+  times = 1000L
+)
 
-zzz <- function(o) {
-  op <- as.name(o)
-  op
+
+e_data3 <- filter_x(e_data, "sum", "sample", "==", c(5, 7), setop = "xor")
+
+e_data2 <- e_data[aaa&bbb, , drop = FALSE]
+
+test <- function(a, b, c) {
+  print(any(lengths(list(a, b, c)) > 1))
 }
-zzz("<")
+
+test(NULL, 1, 1)
+
+any(lengths(list(1:3, 4:9, 1)))
+
+# example of the output from: op(test_values, value).
+# You'll need to 'apply' it to get multiple vectors
+ccc <- c(F, T, T)
+ddd <- c(T, F, T)
+
+ccc & ddd # intersection
+ccc | ddd # union
+xor(ccc, ddd) # setdiff
+
+# Then just subset the matrix a single time with the output vector!!!!
